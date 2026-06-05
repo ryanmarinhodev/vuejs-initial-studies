1594143bbb821639c2a10848c04590b81739bfa9
ryanmarinhodev <ryanmarinhodev@gmail.com>
2026-06-05 16:27:03 -0300

    vueJS pratice

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const message = ref('First VueJs contact');
+const count = ref(0);
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Hello world!" />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+  <h1>Outra mensagem</h1>
+  <div>
+    <button @click="count++">{{ count }}</button>
+    <button @click="count = 0">Limpar</button>
+  </div>
 </template>
 
 <style scoped>
 header {
   line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
 }
 
 @media (min-width: 1024px) {
